f1e8cb9461a54562e5162736642ebd4b0eb11745
ApsanP <Apsan290@gmail.com>
2024-07-03 19:51:27 +1200

    !

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 8080;
 
 app.get("/", (req, res) => {
-  res.send(`Hello You we are on AWS!.. ${process.env.HELLO_WORLD} ${process.env.API_SECRET}`);
+  res.send(`Hello You we are on AWS!..! ${process.env.HELLO_WORLD} ${process.env.API_SECRET}`);
 });
 
 app.listen(port, () => {
